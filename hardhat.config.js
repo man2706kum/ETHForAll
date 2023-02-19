6002e4229e4e332e172322f5e08a7c1fa2f01d06
Manish <kumarmanish19963@gmail.com>
2023-02-19 23:40:37 +0530

    deployed on mumbai-testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    "mantle-testnet": {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [process.env.PRIV_KEY] // Uses the private key from the .env file
+    }
+  }
 };
